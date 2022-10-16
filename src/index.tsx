c5922076f78952afb1dc5af8d38d0e51fad5623c
Fuji-P <nyaguri0417@gmail.com>
2022-10-16 21:28:20 +0900

    3 React/Next.jsの基礎 3.1.2/React の基本

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';    //App.jsからApp関数を取り込んでいる
+import Hello from './components/Hello';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(<Hello />);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
