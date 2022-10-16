9cddaa4dd4da7feb851afd9ffc734e1e7795f14b
Fuji-P <nyaguri0417@gmail.com>
2022-10-16 22:21:28 +0900

    3 React/Next.jsの基礎 3.2.2/コンポーネント(Reactコンポーネント) Parent

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,14 @@ import './index.css';
 //import App from './App';    //App.jsからApp関数を取り込んでいる
 //import Hello from './components/Hello';
 //import Name from './components/Name';
-import Message from './components/Message';
+//import Message from './components/Message';
+import Parent from './components/ContainerSample';
 import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
-  root.render(<Message />);
+  root.render(<Parent />);
 }
 
 // If you want to start measuring performance in your app, pass a function
